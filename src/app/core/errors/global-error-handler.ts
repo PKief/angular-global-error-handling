c275d5177891e6ea2cd399a500bf42cf1ab8fe14
Thomas Turrell-Croft <thomasturrell@users.noreply.github.com>
2020-12-21 12:13:29 +0100

    Handle errors that occur outside of Angular Zone
    Fixes issue where it is not possible to close dialog if error was thrown outside of the Angular Zone. For instance errors thrown in ngOnInit occur outside of the Angular Zone.

--- src/app/core/errors/global-error-handler.ts
+++ src/app/core/errors/global-error-handler.ts
@@ -1,14 +1,17 @@
-import { ErrorHandler, Injectable } from "@angular/core";
+import { ErrorHandler, Injectable, NgZone } from "@angular/core";
 import { ErrorDialogService } from "../../shared/errors/error-dialog.service";
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
-  constructor(private errorDialogService: ErrorDialogService) {}
+  constructor(private errorDialogService: ErrorDialogService, private zone: NgZone) {}
 
   handleError(error: Error) {
-    this.errorDialogService.openDialog(
-      error.message || "Undefined client error"
-    );
+
+    this.zone.run(() =>
+      this.errorDialogService.openDialog(
+        error.message || "Undefined client error"
+    ));
+
     console.error("Error from global error handler", error);
   }
 }
