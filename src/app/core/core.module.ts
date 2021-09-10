0ae1f62f217aff06d78bc8e7a1c785b6a3a71e3f
Philipp Kief <philipp.kief@gmx.de>
2021-09-10 22:54:02 +0200

    Rename interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { GlobalErrorHandler } from './errors/global-error-handler';
-import { HttpLoadingInterceptor } from './errors/http.interceptor';
+import { HttpLoadingInterceptor } from './errors/http-loading.interceptor';
 
 @NgModule({
   declarations: [],
